75af8236181d907f178062f6c641f7d6898686d9
Jacek <jacek.chrapkowski@intel.com>
2019-01-23 18:37:44 +0100

    Links update

--- docs/user-guide-html/Data/Search.js
+++ docs/user-guide-html/Data/Search.js
@@ -1,1 +1,1 @@
-define({r:0.5,st:'Stem',n:1,t:[0],u:['../Content/NAUTA/accessing_kubernetes.htm'],s:['$','offici'],p:[['$',4],['cleans',4],['experiments',12],['largely',5],['only',35],['sends',24],['to',5]]});
+define({r:0.5,st:'Stem',n:1,t:[0],u:['../Content/NAUTA V1.0/accessing_kubernetes.htm'],s:['$','offici'],p:[['$',4],['cleans',4],['experiments',12],['largely',5],['only',35],['sends',24],['to',5]]});
